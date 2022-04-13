6dc6688f8db7bb2e65f68c5695c6d1fa40181174
Mr.chu <chuhongkyu@gmail.com>
2022-04-13 16:39:40 +0900

    update

--- js/index.js
+++ js/index.js
@@ -11,22 +11,23 @@ function currentSlide(n) {
 
 function showSlides(n) {
     let i;
-    let slides = document.getElementsByClassName("slide_Box");
+    let slide_Box = document.getElementsByClassName("slide_Box");
     let dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {
+
+    if (n > slide_Box.length) {
         slideIndex = 1
     }
     if (n < 1) {
-        slideIndex = slides.length
+        slideIndex = slide_Box.length
     }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+    for (i = 0; i < slide_Box.length; i++) {
+        slide_Box[i].style.display = "none";
     }
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i]
             .className
             .replace(" active", "");
     }
-    slides[slideIndex - 1].style.display = "block";
+    slide_Box[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
 }

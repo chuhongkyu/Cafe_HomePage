98bfa94c861863a36e0f82866a1cd335c2109277
Mr.chu <chuhongkyu@gmail.com>
2022-04-22 17:30:08 +0900

    make_hashtagbtn&&amke_anim

---
+++ js/findHashtag.js
@@ -0,0 +1,15 @@
+let hashtagList = document.querySelectorAll(".article_contents");
+
+function findHashtag(hash){
+    for (let i = 0; i < hashtagList.length; i++) {
+       if(hashtagList[i].classList.contains(hash) === true){
+           hashtagList[i].style.display = 'flex';
+           console.log("해당 요소");
+       }else{
+           hashtagList[i].style.display = 'none';
+           console.log("해당 제외요소"); 
+       }
+       
+    }
+}
+

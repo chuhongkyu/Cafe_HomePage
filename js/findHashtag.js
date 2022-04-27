b7ef77c73869594c5fdb0c19f59e67ab0c70630c
Mr.chu <chuhongkyu@gmail.com>
2022-04-27 17:52:02 +0900

    josuk
    주석 제작

--- js/findHashtag.js
+++ js/findHashtag.js
@@ -1,18 +1,21 @@
-let hashtagList = document.querySelectorAll(".article_contents");
-let hashbtn = document.querySelectorAll(".hastag_btn");
+const hashTagList = document.querySelectorAll(".article_contents");
+const hashBtn = document.querySelectorAll(".hashtag_btn");
 
 function findHashtag(hash, item){
-    for (let j = 0; j < hashbtn.length; j++) {
-        hashbtn[j].classList.remove("hashtag_click");
+    for (let j = 0; j < hashBtn.length; j++) {
+        hashBtn[j].classList.remove("hashtag_click");
     }
-    for (let i = 0; i < hashtagList.length; i++) {
-       if(hashtagList[i].classList.contains(hash) === true){
-           hashtagList[i].style.display = 'flex';
-        //    console.log("해당 요소");
+    // 버튼을 전부 기본 색으로 바꾼다는 뜻.
+
+    for (let i = 0; i < hashTagList.length; i++) {
+       if(hashTagList[i].classList.contains(hash) === true){
+           hashTagList[i].style.display = 'flex';
+            ///그것만 보여줘
        }else{
-           hashtagList[i].style.display = 'none';
-        //    console.log("해당 제외요소");
+           hashTagList[i].style.display = 'none';
+           ///아닌거 감춰
        }
     }
     item.classList.add("hashtag_click");
+    // 선택한 친구 색 바꾼다는 뜻.
 }
